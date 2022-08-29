4e816e5fcdf42f0599bfa27bee8dc2fdb64142b6
Sedrick Penate <digitalphenom0@gmail.com>
2022-08-29 13:43:06 -0700

    regex complete

---
+++ regular_expressions/start_with?_end_with?.rb
@@ -0,0 +1,14 @@
+
+
+phrase = "Ruby is a Cool Programming Language"
+
+puts phrase.start_with?("Ruby sox")
+
+#----------------------
+
+words = "these are words"
+
+puts words.upcase.start_with?("these")
+
+
+
